6ef7e0c242a0ed267b05c7f6f723e37fcc8eed64
Fernando De La Madrid <fernando.delamadrid@checkr.com>
2025-02-16 07:39:20 -0800

    remove supabase log

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-console.log('supabaseUrl', supabaseUrl);
-
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing environment variables for Supabase')
 }
